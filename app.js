9223fbf90c655fe895e5b14d3b02e414b500c2e7
Eliav Maman <eliav@myinpact.com>
2015-05-22 23:31:09 +0300

    fix myhang

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-//var logger = winston = require('winston');
+var logger = winston = require('winston');
 var express = require('express');
 
-//winston.remove(winston.transports.Console);
-//winston.add(winston.transports.Console, {colorize: true});
+winston.remove(winston.transports.Console);
+winston.add(winston.transports.Console, {colorize: true});
 
 var app = express();
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/public'));
 app.use(function(req, res) {
-  //logger.warn('unknown endpoint: ' + req.originalUrl);
+  logger.warn('unknown endpoint: ' + req.originalUrl);
   res.status(404).end();
 });
 
 var server = app.listen(process.env.PORT || 4567, function () {
-  //logger.info('opsonatus up & running on port %d!', server.address().port);
+  logger.info('opsonatus up & running on port %d!', server.address().port);
 });
-
-
